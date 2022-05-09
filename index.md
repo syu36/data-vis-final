1c18056684f1f89d147bd0e28708e21517622361
syu <syu36@usfca.edu>
2022-05-09 02:16:39 -0700

    add radar chart to site

--- index.md
+++ index.md
@@ -51,6 +51,7 @@ of the top games, using data collected from one of the top board game listing/ra
 <script src="https://d3js.org/d3.v7.min.js"></script>
 
 <div id="linechart"></div>
+<div id="radar"></div>
 
 <script>
 let canvasWidth1 = 1200;
@@ -216,7 +217,6 @@ counts.forEach(count => {
         .style("position", "absolute");
 
     function hover(event, elem) {
-        let coords = d3.pointer(event);
         tooltip.style("left", event.pageX + 10 + "px")
             .style("top", event.pageY - 30 + "px")
             .style("opacity", 1)
@@ -261,6 +261,157 @@ counts.forEach(count => {
         .on("mouseover", hover)
         .on("mouseout", mouseout)
 });
+</script>
+<script>
+let canvasWidth2 = 1200;
+let canvasHeight2 = 1200;
+let width2 = canvasWidth2 - xMargin;
+let height2 = canvasHeight2 - yMargin;
+
+let svg2 = d3.select("div#radar").append("svg")
+    .attr("width", width2)
+    .attr("height", height2);
+
+svg2.append("rect")
+    .attr("fill", "rgb(240, 240, 240)")
+    .attr("width", "100%")
+    .attr("height", "100%");
+
+svg2.append("text")
+    .attr("class", "title")
+    .attr("transform", "translate(" + (width2 / 2) + ", " + yMargin / 2 + ")")
+    .text("Number of Games Owned by the BGG Community")
+
+let radialScale = d3.scaleLinear()
+    .range([0, 250]);
+
+let colorScale = d3.scaleOrdinal()
+    .range(["darkorange", "gray", "navy"])
+
+let rowConverter2 = function(d) {
+    return {
+        num_owned: +d["Owned Users"],
+        domain: d["Domains"].trim(),
+        age: +d["Min Age"]
+    };
+};
+
+d3.csv("bgg_data_domains.csv", rowConverter2).then(data => {
+    let groupedData = d3.flatRollup(data, v => d3.sum(v, d => d.num_owned), d => {
+        if (d.age >= 0 && d.age <= 5) {
+            return "0-5";
+        } else if (d.age >= 6 && d.age <= 11) {
+            return "6-11";
+        } else {
+            return "12+";
+        }
+    }, d => d.domain);
+
+    colorScale.domain(["12+", "6-11", "0-5"]);
+
+    groupedData = groupedData.filter(item => item[1] !== "");
+
+    groupedData = groupedData.sort(function(a, b) {
+        const nameA = a[1].toUpperCase();
+        const nameB = b[1].toUpperCase();
+        if (nameA < nameB) {
+            return 1;
+        }
+        if (nameA > nameB) {
+            return -1;
+        }
+        return 0;
+    });
+
+    let ageGroupedData = d3.groups(groupedData, d => d[0]);
+
+    let features = ageGroupedData[0][1].map(d => d[1]);
+
+    let ticks = [1600000, 3200000, 4800000, 6400000, 8000000];
+    radialScale.domain([0, 8000000]);
+
+    ticks.forEach(t =>
+        svg2.append("circle")
+            .attr("cx", width2 / 2)
+            .attr("cy", height2 / 2)
+            .attr("fill", "none")
+            .attr("stroke", "gray")
+            .attr("r", radialScale(t))
+    );
+
+    ticks.forEach(t =>
+        svg2.append("text")
+            .attr("class", "num-label")
+            .attr("x", width2 / 2 + 5)
+            .attr("y", width2 / 2 - radialScale(t))
+            .text(t.toString())
+    );
+
+    function angleToCoordinate(angle, value) {
+        let x = Math.cos(angle) * radialScale(value);
+        let y = Math.sin(angle) * radialScale(value);
+        return {"x": width2 / 2 + x, "y": width2 / 2 - y};
+    }
+
+    for (let i = 0; i < features.length; i++) {
+        let ftName = features[i];
+        let angle = (Math.PI / 2) + (2 * Math.PI * i / features.length);
+        let lineCoordinate = angleToCoordinate(angle, 8000000);
+        let labelCoordinate = angleToCoordinate(angle, 9550000);
+
+        svg2.append("line")
+            .attr("x1", width2 / 2)
+            .attr("y1", width2 / 2)
+            .attr("x2", lineCoordinate.x)
+            .attr("y2", lineCoordinate.y)
+            .attr("stroke", "black");
+
+        svg2.append("text")
+            .attr("class", "radar-label")
+            .attr("x", labelCoordinate.x)
+            .attr("y", labelCoordinate.y)
+            .text(ftName);
+    }
+
+    let line = d3.line()
+        .x(d => d.x)
+        .y(d => d.y);
+
+    function getPathCoordinates(dataPoint) {
+        let coordinates = [];
+        for (let i = 0; i < features.length; i++) {
+            let angle = (Math.PI / 2) + (2 * Math.PI * i / features.length);
+            coordinates.push(angleToCoordinate(angle, dataPoint[1][i][2]))
+        }
+        return coordinates;
+    }
+
+    for (let i = 0; i < ageGroupedData.length; i++) {
+        let d = ageGroupedData[i];
+        let color = colorScale(ageGroupedData[i][0]);
+        let coordinates = getPathCoordinates(d);
+
+        svg2.append("path")
+            .datum(coordinates)
+            .attr("d", line)
+            .attr("stroke-width", 3)
+            .attr("stroke", color)
+            .attr("fill", color)
+            .attr("stroke-opacity", 1)
+            .attr("opacity", 0.5);
+    }
+
+    svg2.append("g")
+        .attr("class", "legendOrdinal")
+        .attr("transform", "translate(20," + yMargin + ")");
+
+    let legendOrdinal = d3.legendColor()
+        .title("Minimum Recommend Age of Game")
+        .scale(colorScale);
+
+    svg2.select(".legendOrdinal")
+        .call(legendOrdinal);
+})
 </script>
 
 This is the end of the page.
--- radar.js
+++ radar.js
@@ -1,18 +1,29 @@
-let width = 800;
-let height = 800;
+let canvasWidth2 = 1200;
+let canvasHeight2 = 1200;
+let width2 = canvasWidth2 - xMargin;
+let height2 = canvasHeight2 - yMargin;
 
-let svg = d3.select("div#radarchart").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+let svg2 = d3.select("body").append("svg")
+    .attr("width", width2)
+    .attr("height", height2);
 
-svg.append("text")
-    .attr("transform", "translate(" + (width / 7) + ", 20)")
-    .text("Number of Games Owned by Domain and Minimum Age Recommendation");
+svg2.append("rect")
+    .attr("fill", "rgb(240, 240, 240)")
+    .attr("width", "100%")
+    .attr("height", "100%");
+
+svg2.append("text")
+    .attr("class", "title")
+    .attr("transform", "translate(" + (width2 / 2) + ", " + yMargin / 2 + ")")
+    .text("Number of Games Owned by the BGG Community")
 
 let radialScale = d3.scaleLinear()
     .range([0, 250]);
 
-let rowConverter = function(d) {
+let colorScale = d3.scaleOrdinal()
+    .range(["darkorange", "gray", "navy"])
+
+let rowConverter2 = function(d) {
     return {
         num_owned: +d["Owned Users"],
         domain: d["Domains"].trim(),
@@ -20,7 +31,7 @@ let rowConverter = function(d) {
     };
 };
 
-d3.csv("bgg_data_domains.csv", rowConverter).then(data => {
+d3.csv("bgg_data_domains.csv", rowConverter2).then(data => {
     let groupedData = d3.flatRollup(data, v => d3.sum(v, d => d.num_owned), d => {
         if (d.age >= 0 && d.age <= 5) {
             return "0-5";
@@ -29,13 +40,15 @@ d3.csv("bgg_data_domains.csv", rowConverter).then(data => {
         } else {
             return "12+";
         }
-    }, d => d.domain);
+    }, d => d.domain)
 
-    groupedData = groupedData.filter(item => item[1] !== "");
+    colorScale.domain(["12+", "6-11", "0-5"])
+
+    groupedData = groupedData.filter(item => item[1] !== "")
 
     groupedData = groupedData.sort(function(a, b) {
-        const nameA = a[1].toUpperCase();
-        const nameB = b[1].toUpperCase();
+        const nameA = a[1].toUpperCase()
+        const nameB = b[1].toUpperCase()
         if (nameA < nameB) {
             return 1;
         }
@@ -45,34 +58,34 @@ d3.csv("bgg_data_domains.csv", rowConverter).then(data => {
         return 0;
     });
 
-    let ageGroupedData = d3.groups(groupedData, d => d[0]);
+    let ageGroupedData = d3.groups(groupedData, d => d[0])
 
-    let features = ageGroupedData[0][1].map(d => d[1]);
+    let features = ageGroupedData[0][1].map(d => d[1])
 
     let ticks = [1600000, 3200000, 4800000, 6400000, 8000000];
     radialScale.domain([0, 8000000]);
 
     ticks.forEach(t =>
-        svg.append("circle")
-            .attr("cx", 350)
-            .attr("cy", 350)
+        svg2.append("circle")
+            .attr("cx", width2 / 2)
+            .attr("cy", height2 / 2)
             .attr("fill", "none")
             .attr("stroke", "gray")
             .attr("r", radialScale(t))
-    );
+    )
 
     ticks.forEach(t =>
-        svg.append("text")
+        svg2.append("text")
             .attr("class", "num-label")
-            .attr("x", 355)
-            .attr("y", 350 - radialScale(t))
+            .attr("x", width2 / 2 + 5)
+            .attr("y", width2 / 2 - radialScale(t))
             .text(t.toString())
     );
 
     function angleToCoordinate(angle, value) {
         let x = Math.cos(angle) * radialScale(value);
         let y = Math.sin(angle) * radialScale(value);
-        return {"x": 350 + x, "y": 350 - y};
+        return {"x": width2 / 2 + x, "y": width2 / 2 - y};
     }
 
     for (let i = 0; i < features.length; i++) {
@@ -81,25 +94,23 @@ d3.csv("bgg_data_domains.csv", rowConverter).then(data => {
         let lineCoordinate = angleToCoordinate(angle, 8000000);
         let labelCoordinate = angleToCoordinate(angle, 9550000);
 
-        svg.append("line")
-            .attr("x1", 350)
-            .attr("y1", 350)
+        svg2.append("line")
+            .attr("x1", width2 / 2)
+            .attr("y1", width2 / 2)
             .attr("x2", lineCoordinate.x)
             .attr("y2", lineCoordinate.y)
             .attr("stroke", "black");
 
-        svg.append("text")
-            .attr("class", "label")
+        svg2.append("text")
+            .attr("class", "radar-label")
             .attr("x", labelCoordinate.x)
             .attr("y", labelCoordinate.y)
-            .text(ftName);
+            .text(ftName)
     }
 
     let line = d3.line()
         .x(d => d.x)
         .y(d => d.y);
-
-    let colors = ["darkorange", "gray", "navy"];
 
     function getPathCoordinates(dataPoint) {
         let coordinates = [];
@@ -112,10 +123,10 @@ d3.csv("bgg_data_domains.csv", rowConverter).then(data => {
 
     for (let i = 0; i < ageGroupedData.length; i++) {
         let d = ageGroupedData[i];
-        let color = colors[i];
-        let coordinates = getPathCoordinates(d);
+        let color = colorScale(ageGroupedData[i][0]);
+        let coordinates = getPathCoordinates(d)
 
-        svg.append("path")
+        svg2.append("path")
             .datum(coordinates)
             .attr("d", line)
             .attr("stroke-width", 3)
@@ -124,4 +135,15 @@ d3.csv("bgg_data_domains.csv", rowConverter).then(data => {
             .attr("stroke-opacity", 1)
             .attr("opacity", 0.5)
     }
+
+    svg2.append("g")
+        .attr("class", "legendOrdinal")
+        .attr("transform", "translate(20," + yMargin + ")");
+
+    let legendOrdinal = d3.legendColor()
+        .title("Minimum Recommend Age of Game")
+        .scale(colorScale);
+
+    svg2.select(".legendOrdinal")
+        .call(legendOrdinal);
 })
